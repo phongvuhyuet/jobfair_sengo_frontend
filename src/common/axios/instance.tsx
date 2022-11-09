7a32b77b1bb662f07b3f14cffdfdac1ba5173fa3
tyrion <vu.van.phong@moneyforward.vn>
2022-11-09 21:03:02 +0700

    hotfix: swagger delete fail

--- src/common/axios/instance.tsx
+++ src/common/axios/instance.tsx
@@ -5,9 +5,18 @@ import { serviceOptions } from '../open-api/swagger.gen'
 let instance: AxiosInstance
 const defaultConfig = {}
 
+const addRequestInterceptors = (instance: AxiosInstance) => {
+  instance.interceptors.request.use((config: AxiosRequestConfig) => {
+    if (!config.data) {
+      delete config.data
+    }
+    return config
+  })
+}
 export const injectInstance = (config: AxiosRequestConfig = defaultConfig) => {
   if (!instance) {
     instance = axios.create(config)
+    addRequestInterceptors(instance)
   }
   serviceOptions.axios = instance
   return instance
